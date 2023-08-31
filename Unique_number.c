06cc2d75ad50afe7ed500abb625e0b56d44c6265
Deep Tejaswi Adireddy <119724015+DeepTejaswi315@users.noreply.github.com>
2023-08-31 20:16:37 +0530

    #include<stdio.h> int main() {       int a=0,b=0,c=0,d=0,e=0,f=0,g=0,h=0,i=0,j=0,n,r;       scanf("%d",&n);       while(n!=0)       {           r=n%10;           if(r==0)            {                a++;            }            if(r==1)            {                b++;            }            if(r==2)            {                c++;            }            if(r==3)            {                d++;            }            if(r==4)            {                e++;            }            if(r==5)            {                f++;            }            if(r==6)            {                g++;            }            if(r==7)            {                h++;            }            if(r==8)            {                i++;            }            if(r==9)            {                j++;            }            n=n/10;       }       if(a<2 && b<2 && c<2 && d<2 && e<2 && f<2 && g<2 && h<2 && i<2 && j<2 )       {           printf("Unique Number");       }       else       {           printf("Not Unique Number");       } }

--- Unique_number.c
+++ Unique_number.c
@@ -1,26 +1,59 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-	int n,i,digit;
-	static int arr[10];
-	scanf("%d",&n);
-	while(n>0)
-	{
-		digit=n%10;
-		arr[digit]++;
-		n=n/10;
-	}
-	for(i=0;i<10;i++)
-	{
-		if(arr[i]>1)
-		{
-			printf("Not Unique Number");
-			break;
-		}
-	}
-	if(i==10)
-	{
-		printf("Unique Number");
-	}
-	return 0;
+      int a=0,b=0,c=0,d=0,e=0,f=0,g=0,h=0,i=0,j=0,n,r;
+      scanf("%d",&n);
+      while(n!=0)
+      {
+          r=n%10;
+          if(r==0)
+           {
+               a++;
+           }
+           if(r==1)
+           {
+               b++;
+           }
+           if(r==2)
+           {
+               c++;
+           }
+           if(r==3)
+           {
+               d++;
+           }
+           if(r==4)
+           {
+               e++;
+           }
+           if(r==5)
+           {
+               f++;
+           }
+           if(r==6)
+           {
+               g++;
+           }
+           if(r==7)
+           {
+               h++;
+           }
+           if(r==8)
+           {
+               i++;
+           }
+           if(r==9)
+           {
+               j++;
+           }
+           n=n/10;
+      }
+      if(a<2 && b<2 && c<2 && d<2 && e<2 && f<2 && g<2 && h<2 && i<2 && j<2 )
+      {
+          printf("Unique Number");
+      }
+      else
+      {
+          printf("Not Unique Number");
+      }
 }
